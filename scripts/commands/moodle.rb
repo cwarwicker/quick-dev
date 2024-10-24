5500f03802b7ef35646d19582504ee9523b3cf77
Conn Warwicker <connwarwicker@hotmail.co.uk>
2024-10-24 17:01:03 +0100

    Fix incorrect data dir permissions in moodle image + incorrect path for moodle commands

--- scripts/commands/moodle.rb
+++ scripts/commands/moodle.rb
@@ -1,15 +1,15 @@
 class Moodle
 
     def install(container)
-       system("docker exec -it #{container} php /var/www/site/admin/cli/install_database.php --agree-license --adminuser=admin --adminpass=password --adminemail=admin@local.host")
+       system("docker exec -it #{container} php admin/cli/install_database.php --agree-license --adminuser=admin --adminpass=password --adminemail=admin@local.host")
     end
 
     def purge(container)
-       system("docker exec -it #{container} php /var/www/site/admin/cli/purge_caches.php")
+       system("docker exec -it #{container} php admin/cli/purge_caches.php")
     end
 
     def upgrade(container)
-       system("docker exec -it #{container} php /var/www/site/admin/cli/upgrade.php")
+       system("docker exec -it #{container} php admin/cli/upgrade.php")
     end
 
 end
