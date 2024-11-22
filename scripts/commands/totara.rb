fcfece08165e85fcd16c91e3dc5db8736e8cd079
Conn Warwicker <connwarwicker@hotmail.co.uk>
2024-11-22 16:26:18 +0000

    Add Totara support.

---
+++ scripts/commands/totara.rb
@@ -0,0 +1,17 @@
+require_relative 'moodle.rb'
+
+class Totara < Moodle
+
+    def install(container, qd)
+       system("docker exec -it #{container} php server/admin/cli/install_database.php --agree-license --adminuser=admin --adminpass=totara --adminemail=admin@local.host --fullname=Totara --shortname=Totara")
+    end
+
+    def purge(container, qd)
+       system("docker exec -it #{container} php server/admin/cli/purge_caches.php")
+    end
+
+    def upgrade(container, qd)
+       system("docker exec -it #{container} php server/admin/cli/upgrade.php -y")
+    end
+
+end
