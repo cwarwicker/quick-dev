13173d2bf6bce3a93462b94fe5166e320275f2e7
Conn Warwicker <connwarwicker@hotmail.co.uk>
2024-11-28 16:16:39 +0000

    Fix Totara comand inheritence so we don't duplicate commands from Moodle.

--- scripts/commands/moodle.rb
+++ scripts/commands/moodle.rb
@@ -2,31 +2,33 @@ require_relative 'php.rb'
 
 class Moodle < Php
 
+    RELATIVE_DIR = ""
+
     def install(container, qd)
-       system("docker exec -it #{container} php admin/cli/install_database.php --agree-license --adminuser=admin --adminpass=moodle --adminemail=admin@local.host --fullname=Moodle --shortname=Moodle")
+       system("docker exec -it #{container} php #{self.class::RELATIVE_DIR}/admin/cli/install_database.php --agree-license --adminuser=admin --adminpass=moodle --adminemail=admin@local.host --fullname=Moodle --shortname=Moodle")
     end
 
     def purge(container, qd)
-       system("docker exec -it #{container} php admin/cli/purge_caches.php")
+       system("docker exec -it #{container} php #{self.class::RELATIVE_DIR}admin/cli/purge_caches.php")
     end
 
     def upgrade(container, qd)
-       system("docker exec -it #{container} php admin/cli/upgrade.php --non-interactive")
+       system("docker exec -it #{container} php #{self.class::RELATIVE_DIR}admin/cli/upgrade.php --non-interactive")
     end
 
     def makecourse(container, qd)
        size = ARGV[1]
        name = ARGV[2]
-       system("docker exec -it #{container} php admin/tool/generator/cli/maketestcourse.php --size=#{size} --shortname=#{name} --fullname=#{name}")
+       system("docker exec -it #{container} php #{self.class::RELATIVE_DIR}admin/tool/generator/cli/maketestcourse.php --size=#{size} --shortname=#{name} --fullname=#{name}")
     end
 
     def phpunit(container, qd)
        opt = ARGV[1]
        if opt === "init"
-          system("docker exec -it #{container} php admin/tool/phpunit/cli/init.php")
+          system("docker exec -it #{container} php #{self.class::RELATIVE_DIR}admin/tool/phpunit/cli/init.php")
        else
           opts = ARGV[1..-1].join(' ')
-          system("docker exec -it #{container} php admin/tool/phpunit/cli/util.php --run #{opts}")
+          system("docker exec -it #{container} php #{self.class::RELATIVE_DIR}admin/tool/phpunit/cli/util.php --run #{opts}")
        end
     end
 
