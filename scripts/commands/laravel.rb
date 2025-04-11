2cd0456aa616caeb88d35e2f829dc717be883a7e
Conn Warwicker <connwarwicker@hotmail.co.uk>
2025-04-11 09:14:56 +0100

    Add qd refresh command for laravel to refresh and seed db.

--- scripts/commands/laravel.rb
+++ scripts/commands/laravel.rb
@@ -7,4 +7,9 @@ class Laravel < Php
         system("docker exec -it #{container} php artisan #{cmd}")
     end
 
+    def refresh(container, qd)
+        cmd = ARGV[1..-1].join(' ')
+        system("docker exec -it #{container} bash -c 'php artisan migrate:fresh && php artisan db:seed #{cmd}'")
+    end
+
 end
