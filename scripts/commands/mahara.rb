aa62a91c87a919783417af017b4e2fadda47f66e
Conn Warwicker <conn.warwicker@catalyst-eu.net>
2025-04-28 12:40:41 +0100

    Fixed qd install for mahara to use www-data and changed admin password

--- scripts/commands/mahara.rb
+++ scripts/commands/mahara.rb
@@ -5,7 +5,7 @@ class Mahara < Php
     RELATIVE_DIR = "./htdocs/"
 
     def install(container, qd)
-       system("docker exec -it #{container} php #{self.class::RELATIVE_DIR}admin/cli/install.php --adminpassword=moodle --adminemail=admin@local.host --sitename=Mahara")
+       system("docker exec -it --user www-data #{container} php #{self.class::RELATIVE_DIR}admin/cli/install.php --adminpassword=mahara --adminemail=admin@local.host --sitename=Mahara")
     end
 
 end
