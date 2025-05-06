f2e252af0f5cfd9c0cc071cd7381d80b1667b280
Conn Warwicker <conn.warwicker@catalyst-eu.net>
2025-05-06 15:45:48 +0100

    Add upgrade command to Mahara

--- scripts/commands/mahara.rb
+++ scripts/commands/mahara.rb
@@ -8,4 +8,8 @@ class Mahara < Php
        system("docker exec -it --user www-data #{container} php #{self.class::RELATIVE_DIR}admin/cli/install.php --adminpassword=mahara --adminemail=admin@local.host --sitename=Mahara")
     end
 
+    def upgrade(container, qd)
+        system("docker exec -it --user www-data #{container} php #{self.class::RELATIVE_DIR}admin/cli/upgrade.php")
+     end
+
 end
