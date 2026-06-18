fab090344ae8ea399cc8c0ec000b1de87f3a5ba8
Conn Warwicker <conn.warwicker@catalyst-eu.net>
2026-06-18 16:35:12 +0100

    Add cron and purge commands to mahara, plus 26.04 preset

--- scripts/commands/mahara.rb
+++ scripts/commands/mahara.rb
@@ -4,12 +4,21 @@ class Mahara < Php
 
     RELATIVE_DIR = "./htdocs/"
 
-    def install(container, qd)
-       system("docker exec -it --user www-data #{container} php #{self.class::RELATIVE_DIR}admin/cli/install.php --adminpassword=mahara --adminemail=admin@local.host --sitename=Mahara")
-    end
+   def install(container, qd)
+      system("docker exec -it --user www-data #{container} php #{self.class::RELATIVE_DIR}admin/cli/install.php --adminpassword=mahara --adminemail=admin@local.host --sitename=Mahara")
+   end
 
-    def upgrade(container, qd)
-        system("docker exec -it --user www-data #{container} php #{self.class::RELATIVE_DIR}admin/cli/upgrade.php")
-     end
+   def upgrade(container, qd)
+      system("docker exec -it --user www-data #{container} php #{self.class::RELATIVE_DIR}admin/cli/upgrade.php")
+   end
+
+   def cron(container, qd)
+      opts = ARGV[1..-1].join(' ')
+      system("docker exec -it #{container} php #{self.class::RELATIVE_DIR}/lib/cron.php #{opts}")
+   end
+
+   def purge(container, qd)
+      system("docker exec -it #{container} php #{self.class::RELATIVE_DIR}/admin/cli/clear_caches.php")
+   end
 
 end
